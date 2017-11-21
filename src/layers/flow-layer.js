1063eda8997c86c8abd6332835c6c2dd3726f11e
eeshamkohli <33865971+eeshamkohli@users.noreply.github.com>
2017-11-21 19:27:08 +0530

    Color gradient redefined in colorscale.
    Very light colors which were not completely bisible on the map have been removed

--- src/layers/flow-layer.js
+++ src/layers/flow-layer.js
@@ -20,7 +20,20 @@ let FlowLayer = ForecastLayer.extend({
       minVelocity: 3,           // used to align color scale
       maxVelocity: 20,          // used to align color scale
       velocityScale: 0.01,     // modifier for particle animations, arbitrarily defaults to 0.005
-      colorScale: null,
+      colorScale: [
+        "rgb(36,104, 180)",
+        "rgb(60,157, 194)",
+        "rgb(128,205,193 )",
+        "rgb(151,218,168 )",
+        "rgb(252,217,125)",
+        "rgb(255,182,100)",
+        "rgb(252,150,75)",
+        "rgb(250,112,52)",
+        "rgb(245,64,32)",
+        "rgb(237,45,28)",
+        "rgb(220,24,32)",
+        "rgb(180,0,35)"
+      ],
       data: null                // data will be requested on-demand
     }, options)
     let layer = L.velocityLayer(layerOptions)
